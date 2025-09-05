9a8b49fc150fdfc3393fa9f8830cc74f4ff14107
zeroheight <tokens-management@zeroheight.com>
2025-09-05 21:01:12 +0300

    Update Design Tokens Starter Set | JSON, Variables & Tokens Studio sync (Community) from zeroheight

---
+++ iOS2/token_Alias_DarkMode.swift
@@ -0,0 +1,73 @@
+
+//
+// 
+//
+
+import UIKit
+
+public class DesignTokensStarterSetJSONVariablesTokensStudiosyncCommunity {
+    public static let colorAliasColorBackgroundBody = UIColor(red: 0.110, green: 0.110, blue: 0.118, alpha: 1) /* Body background color */
+    public static let colorAliasColorBackgroundHighEmphasis = UIColor(red: 0.157, green: 0.157, blue: 0.157, alpha: 1) /* Background Color for components with high emphasis */
+    public static let colorAliasColorBackgroundLowEmphasis = UIColor(red: 0.294, green: 0.294, blue: 0.294, alpha: 1) /* background color for elements with a low emphasis */
+    public static let colorAliasColorBackgroundMediumEmphasis = UIColor(red: 0.224, green: 0.224, blue: 0.224, alpha: 1) /* Background Color for components with medium emphasis */
+    public static let colorAliasColorBorderFocusOutline = UIColor(red: 0.098, green: 0.576, blue: 0.816, alpha: 1) /* color for default focus outline */
+    public static let colorAliasColorBorderHeavy = UIColor(red: 0.878, green: 0.878, blue: 0.878, alpha: 1) /* heavy border color */
+    public static let colorAliasColorBorderMedium = UIColor(red: 0.467, green: 0.467, blue: 0.467, alpha: 1) /* medium border color */
+    public static let colorAliasColorBorderSubtle = UIColor(red: 0.294, green: 0.294, blue: 0.294, alpha: 1) /* subtle border color */
+    public static let colorAliasColorPrimaryBrand = UIColor(red: 0.110, green: 0.110, blue: 0.118, alpha: 1) /* background color for primary brand elements */
+    public static let colorAliasColorPrimaryInteraction = UIColor(red: 0.333, green: 0.922, blue: 0.808, alpha: 1) /* Color that indicates interactions */
+    public static let colorAliasColorPrimaryOnBrand = UIColor(red: 0.773, green: 0.973, blue: 0.918, alpha: 1) /* color for elements that are placed on brand background color */
+    public static let colorAliasColorPrimaryOnInteraction = UIColor(red: 0.078, green: 0.173, blue: 0.157, alpha: 1) /* color for elements that are placed on interactive background colors */
+    public static let colorAliasColorPurposeCriticalHighEmphasis = UIColor(red: 0.953, green: 0.478, blue: 0.451, alpha: 1) /* color for elements with critical purpose of high emphasis */
+    public static let colorAliasColorPurposeCriticalLowEmphasis = UIColor(red: 0.729, green: 0.353, blue: 0.337, alpha: 1) /* color for elements with critical purpose of low emphasis */
+    public static let colorAliasColorPurposeInfoHighEmphasis = UIColor(red: 0.584, green: 0.596, blue: 0.878, alpha: 1) /* color for elements with informational purpose of high emphasis */
+    public static let colorAliasColorPurposeInfoLowEmphasis = UIColor(red: 0.816, green: 0.824, blue: 0.953, alpha: 1) /* color for elements with informational purpose of low emphasis */
+    public static let colorAliasColorPurposeOnCritical = UIColor(red: 0.231, green: 0.125, blue: 0.125, alpha: 1) /* color for elements placed on critical purpose background */
+    public static let colorAliasColorPurposeOnInfo = UIColor(red: 0.224, green: 0.204, blue: 0.353, alpha: 1) /* color for elements placed on informational purpose background */
+    public static let colorAliasColorPurposeOnSuccess = UIColor(red: 0.165, green: 0.243, blue: 0.157, alpha: 1) /* color for elements placed on success purpose background */
+    public static let colorAliasColorPurposeOnWarning = UIColor(red: 0.243, green: 0.224, blue: 0.133, alpha: 1) /* color for elements placed on warning purpose background */
+    public static let colorAliasColorPurposeSuccessHighEmphasis = UIColor(red: 0.412, green: 0.678, blue: 0.396, alpha: 1) /* color for elements with success purpose of high emphasis */
+    public static let colorAliasColorPurposeSuccessLowEmphasis = UIColor(red: 0.690, green: 0.882, blue: 0.667, alpha: 1) /* color for elements with success purpose of low emphasis */
+    public static let colorAliasColorPurposeWarningHighEmphasis = UIColor(red: 0.941, green: 0.827, blue: 0.420, alpha: 1) /* color for elements with warning purpose of high emphasis */
+    public static let colorAliasColorPurposeWarningLowEmphasis = UIColor(red: 0.694, green: 0.616, blue: 0.318, alpha: 1) /* color for elements with warning purpose of low emphasis */
+    public static let colorAliasColorStateDisable = UIColor(red: 0.831, green: 0.831, blue: 0.831, alpha: 1) /* background color for disabled elements */
+    public static let colorAliasColorStateFocus = UIColor(red: 0.929, green: 0.929, blue: 0.929, alpha: 0.16) /* Background color for state overlay of a focused element */
+    public static let colorAliasColorStateHover = UIColor(red: 0.929, green: 0.929, blue: 0.929, alpha: 0.16) /* Background color for state overlay of a hovered element */
+    public static let colorAliasColorStateOnDisable = UIColor(red: 0.294, green: 0.294, blue: 0.294, alpha: 1) /* color for elements that are placed on disabled background color */
+    public static let colorAliasColorStateOnSelect = UIColor(red: 0.102, green: 0.416, blue: 0.365, alpha: 1) /* Foreground color for selected elements */
+    public static let colorAliasColorStateOnSelectInverted = UIColor(red: 0.773, green: 0.973, blue: 0.918, alpha: 1) /* inverted color for elements on selected backgrounds */
+    public static let colorAliasColorStatePress = UIColor(red: 0.929, green: 0.929, blue: 0.929, alpha: 0.08) /* Background color for state overlay of a pressed element */
+    public static let colorAliasColorStateSelect = UIColor(red: 0.773, green: 0.973, blue: 0.918, alpha: 1) /* background color for selected elements */
+    public static let colorAliasColorStateSelectInverted = UIColor(red: 0.102, green: 0.416, blue: 0.365, alpha: 1) /* Inverted background color for selected elements */
+    public static let colorAliasColorTextDefault = UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha: 1) /* Default text color */
+    public static let colorAliasColorTextInverted = UIColor(red: 0.157, green: 0.157, blue: 0.157, alpha: 1) /* Inverted text color */
+    public static let colorAliasColorTextSubtle = UIColor(red: 0.620, green: 0.620, blue: 0.620, alpha: 1) /* Text color in subtle weight */
+    public static let contentAliasTypescaleFontFamilyBodyLong = "Inter" /* fontfamily for multi line paragraph texts */
+    public static let contentAliasTypescaleFontFamilyBodyShort = "Inter" /* fontfamily for single line paragraph texts */
+    public static let contentAliasTypescaleFontFamilyBodySmall = "Inter" /* fontfamily for small paragraph texts */
+    public static let contentAliasTypescaleFontFamilyDisplay1 = "Inter" /* fontfamily for display 01 */
+    public static let contentAliasTypescaleFontFamilyDisplay2 = "Inter" /* fontfamily for display 02 */
+    public static let contentAliasTypescaleFontFamilyHeadline1 = "Inter" /* fontfamily for headline 01 */
+    public static let contentAliasTypescaleFontFamilyHeadline2 = "Inter" /* fontfamily for headline 02 */
+    public static let contentAliasTypescaleFontFamilyHeadline3 = "Inter" /* fontfamily for headline 03 */
+    public static let contentAliasTypescaleFontFamilyHeadline4 = "Inter" /* fontweight for headline 04 */
+    public static let contentAliasTypescaleFontFamilyHeadline5 = "Inter" /* fontfamily for headline 05 */
+    public static let contentAliasTypescaleFontFamilyHyperlinkDefault = "Inter" /* fontfamily for hyperlinks */
+    public static let contentAliasTypescaleFontFamilyHyperlinkHover = "Inter" /* fontfamily for hyperlinks in hover state */
+    public static let contentAliasTypescaleFontFamilyLabelLong = "Inter" /* fontfamily for multi line labels */
+    public static let contentAliasTypescaleFontFamilyLabelShort = "Inter" /* fontfamily for single line labels */
+    public static let contentAliasTypescaleFontWeightBodyLong = "Regular" /* font weight for multi line paragraph texts */
+    public static let contentAliasTypescaleFontWeightBodyShort = "Regular" /* font weight for single line paragraph texts */
+    public static let contentAliasTypescaleFontWeightBodySmall = "Regular" /* font weight for small paragraph texts */
+    public static let contentAliasTypescaleFontWeightDisplay1 = "Black" /* font weight for display 01 */
+    public static let contentAliasTypescaleFontWeightDisplay2 = "Bold" /* font weight for display 02 */
+    public static let contentAliasTypescaleFontWeightHeadline1 = "Black" /* font weight for headline 01 */
+    public static let contentAliasTypescaleFontWeightHeadline2 = "Bold" /* font weight for headline 02 */
+    public static let contentAliasTypescaleFontWeightHeadline3 = "Bold" /* font weight for headline 03 */
+    public static let contentAliasTypescaleFontWeightHeadline4 = "Bold" /* font weight for headline 04 */
+    public static let contentAliasTypescaleFontWeightHeadline5 = "Bold" /* font weight for headline 05 */
+    public static let contentAliasTypescaleFontWeightHyperlinkDefault = "Regular" /* font weight for hyperlinks */
+    public static let contentAliasTypescaleFontWeightHyperlinkHover = "Regular" /* font weight for hyperlinks in hover state */
+    public static let contentAliasTypescaleFontWeightLabelLong = "Bold" /* font weight for multi line labels */
+    public static let contentAliasTypescaleFontWeightLabelShort = "Semi Bold" /* font weight for single line  labels */
+}
