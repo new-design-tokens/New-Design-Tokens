1704a51c0e54354b1394756d601aa29e453db7be
zeroheight <tokens-management@zeroheight.com>
2025-09-05 21:23:39 +0300

    Update Design Tokens Starter Set | JSON, Variables & Tokens Studio sync (Community) from zeroheight

---
+++ Flutter3/token_Components_SectionMessage.dart
@@ -0,0 +1,18 @@
+
+//
+// 
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const colorComponentSectionMessageSuccessBackground = Color(0xFF69AD65); /* background color for success section messages */
+    static const colorComponentSectionMessageSuccessIcon = Color(0xFF2A3E28); /* icon color for success section messages */
+    static const colorComponentSectionMessageSuccessText = Color(0xFF2A3E28); /* text color for success section messages */
+    static const colorComponentSectionMessageWarningBackground = Color(0xFFF0D36B); /* background color for warning section messages */
+    static const colorComponentSectionMessageWarningIcon = Color(0xFF3E3922); /* icon color for warning section messages */
+    static const colorComponentSectionMessageWarningText = Color(0xFF3E3922); /* text color for warning section messages */
+}
