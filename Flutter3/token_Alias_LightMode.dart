0b277bdb0f6e7d2d6219baddd7f3658da0c196bc
zeroheight <tokens-management@zeroheight.com>
2025-09-05 21:23:37 +0300

    Update Design Tokens Starter Set | JSON, Variables & Tokens Studio sync (Community) from zeroheight

---
+++ Flutter3/token_Alias_LightMode.dart
@@ -0,0 +1,76 @@
+
+//
+// 
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const colorAliasColorBackgroundBody = Color(0xFFEDEDED); /* Body background color */
+    static const colorAliasColorBackgroundHighEmphasis = Color(0xFFFCFCFC); /* Background Color for components with high emphasis */
+    static const colorAliasColorBackgroundLowEmphasis = Color(0xFFD4D4D4); /* background color for elements with a low emphasis */
+    static const colorAliasColorBackgroundMediumEmphasis = Color(0xFFE0E0E0); /* Background Color for components with medium emphasis */
+    static const colorAliasColorBorderFocusOutline = Color(0xFF686FC8); /* color for default focus outline */
+    static const colorAliasColorBorderHeavy = Color(0xFFE0E0E0); /* heavy border color */
+    static const colorAliasColorBorderMedium = Color(0xFF777777); /* medium border color */
+    static const colorAliasColorBorderSubtle = Color(0xFF4B4B4B); /* subtle border color */
+    static const colorAliasColorPrimaryBrand = Color(0xFF1C1C1E); /* background color for primary brand elements */
+    static const colorAliasColorPrimaryInteraction = Color(0xFF00D4B6); /* Color that indicates interactions */
+    static const colorAliasColorPrimaryOnBrand = Color(0xFFC5F8EA); /* color for elements that are placed on brand background color */
+    static const colorAliasColorPrimaryOnInteraction = Color(0xFFC5F8EA); /* color for elements that are placed on interactive background colors */
+    static const colorAliasColorPurposeCriticalHighEmphasis = Color(0xFFF37A73); /* color for elements with critical purpose of high emphasis */
+    static const colorAliasColorPurposeCriticalLowEmphasis = Color(0xFFBA5A56); /* color for elements with critical purpose of low emphasis */
+    static const colorAliasColorPurposeInfoHighEmphasis = Color(0xFF9598E0); /* color for elements with informational purpose of high emphasis */
+    static const colorAliasColorPurposeInfoLowEmphasis = Color(0xFFD0D2F3); /* color for elements with informational purpose of low emphasis */
+    static const colorAliasColorPurposeOnCritical = Color(0xFF3B2020); /* color for elements placed on critical purpose background */
+    static const colorAliasColorPurposeOnInfo = Color(0xFF39345A); /* color for elements placed on informational purpose background */
+    static const colorAliasColorPurposeOnSuccess = Color(0xFF2A3E28); /* color for elements placed on success purpose background */
+    static const colorAliasColorPurposeOnWarning = Color(0xFF3E3922); /* color for elements placed on warning purpose background */
+    static const colorAliasColorPurposeSuccessHighEmphasis = Color(0xFF69AD65); /* color for elements with success purpose of high emphasis */
+    static const colorAliasColorPurposeSuccessLowEmphasis = Color(0xFFB0E1AA); /* color for elements with success purpose of low emphasis */
+    static const colorAliasColorPurposeWarningHighEmphasis = Color(0xFFF0D36B); /* color for elements with warning purpose of high emphasis */
+    static const colorAliasColorPurposeWarningLowEmphasis = Color(0xFFB19D51); /* color for elements with warning purpose of low emphasis */
+    static const colorAliasColorStateDisable = Color(0xFFD4D4D4); /* background color for disabled elements */
+    static const colorAliasColorStateFocus = Color(0x29EDEDED); /* Background color for state overlay of a focused element */
+    static const colorAliasColorStateHover = Color(0x29EDEDED); /* Background color for state overlay of a hovered element */
+    static const colorAliasColorStateOnDisable = Color(0xFF4B4B4B); /* color for elements that are placed on disabled background color */
+    static const colorAliasColorStateOnSelect = Color(0xFF1A6A5D); /* Foreground color for selected elements */
+    static const colorAliasColorStateOnSelectInverted = Color(0xFFC5F8EA); /* inverted color for elements on selected backgrounds */
+    static const colorAliasColorStatePress = Color(0x14EDEDED); /* Background color for state overlay of a pressed element */
+    static const colorAliasColorStateSelect = Color(0xFFC5F8EA); /* background color for selected elements */
+    static const colorAliasColorStateSelectInverted = Color(0xFF1A6A5D); /* Inverted background color for selected elements */
+    static const colorAliasColorTextDefault = Color(0xFF282828); /* Default text color */
+    static const colorAliasColorTextInverted = Color(0xFFFCFCFC); /* Inverted text color */
+    static const colorAliasColorTextSubtle = Color(0xFF5E5E5E); /* Text color in subtle weight */
+    static const contentAliasTypescaleFontFamilyBodyLong = "Inter"; /* fontfamily for multi line paragraph texts */
+    static const contentAliasTypescaleFontFamilyBodyShort = "Inter"; /* fontfamily for single line paragraph texts */
+    static const contentAliasTypescaleFontFamilyBodySmall = "Inter"; /* fontfamily for small paragraph texts */
+    static const contentAliasTypescaleFontFamilyDisplay1 = "Inter"; /* fontfamily for display 01 */
+    static const contentAliasTypescaleFontFamilyDisplay2 = "Inter"; /* fontfamily for display 02 */
+    static const contentAliasTypescaleFontFamilyHeadline1 = "Inter"; /* fontfamily for headline 01 */
+    static const contentAliasTypescaleFontFamilyHeadline2 = "Inter"; /* fontfamily for headline 02 */
+    static const contentAliasTypescaleFontFamilyHeadline3 = "Inter"; /* fontfamily for headline 03 */
+    static const contentAliasTypescaleFontFamilyHeadline4 = "Inter"; /* fontweight for headline 04 */
+    static const contentAliasTypescaleFontFamilyHeadline5 = "Inter"; /* fontfamily for headline 05 */
+    static const contentAliasTypescaleFontFamilyHyperlinkDefault = "Inter"; /* fontfamily for hyperlinks */
+    static const contentAliasTypescaleFontFamilyHyperlinkHover = "Inter"; /* fontfamily for hyperlinks in hover state */
+    static const contentAliasTypescaleFontFamilyLabelLong = "Inter"; /* fontfamily for multi line labels */
+    static const contentAliasTypescaleFontFamilyLabelShort = "Inter"; /* fontfamily for single line labels */
+    static const contentAliasTypescaleFontWeightBodyLong = "Regular"; /* font weight for multi line paragraph texts */
+    static const contentAliasTypescaleFontWeightBodyShort = "Regular"; /* font weight for single line paragraph texts */
+    static const contentAliasTypescaleFontWeightBodySmall = "Regular"; /* font weight for small paragraph texts */
+    static const contentAliasTypescaleFontWeightDisplay1 = "Black"; /* font weight for display 01 */
+    static const contentAliasTypescaleFontWeightDisplay2 = "Bold"; /* font weight for display 02 */
+    static const contentAliasTypescaleFontWeightHeadline1 = "Black"; /* font weight for headline 01 */
+    static const contentAliasTypescaleFontWeightHeadline2 = "Bold"; /* font weight for headline 02 */
+    static const contentAliasTypescaleFontWeightHeadline3 = "Bold"; /* font weight for headline 03 */
+    static const contentAliasTypescaleFontWeightHeadline4 = "Bold"; /* font weight for headline 04 */
+    static const contentAliasTypescaleFontWeightHeadline5 = "Bold"; /* font weight for headline 05 */
+    static const contentAliasTypescaleFontWeightHyperlinkDefault = "Regular"; /* font weight for hyperlinks */
+    static const contentAliasTypescaleFontWeightHyperlinkHover = "Regular"; /* font weight for hyperlinks in hover state */
+    static const contentAliasTypescaleFontWeightLabelLong = "Bold"; /* font weight for multi line labels */
+    static const contentAliasTypescaleFontWeightLabelShort = "Semi Bold"; /* font weight for single line  labels */
+}
