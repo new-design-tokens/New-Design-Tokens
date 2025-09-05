61cb72f114d070bef8fbe1dfaf1cf589519a8ef2
zeroheight <tokens-management@zeroheight.com>
2025-09-05 21:08:53 +0300

    Update Design Tokens Starter Set | JSON, Variables & Tokens Studio sync (Community) from zeroheight

---
+++ iOS2/token_Global_Brand.swift
@@ -0,0 +1,96 @@
+
+//
+// 
+//
+
+import UIKit
+
+public class DesignTokensStarterSetJSONVariablesTokensStudiosyncCommunity {
+    public static let colorGlobalColorBlue100 = UIColor(red: 0.878, green: 0.941, blue: 0.976, alpha: 1) /* global color blue of weight 100 */
+    public static let colorGlobalColorBlue200 = UIColor(red: 0.694, green: 0.859, blue: 0.937, alpha: 1) /* global color blue of weight 200 */
+    public static let colorGlobalColorBlue300 = UIColor(red: 0.518, green: 0.773, blue: 0.902, alpha: 1) /* global color blue of weight 300 */
+    public static let colorGlobalColorBlue400 = UIColor(red: 0.325, green: 0.686, blue: 0.863, alpha: 1) /* global color blue of weight 400 */
+    public static let colorGlobalColorBlue50 = UIColor(red: 0.965, green: 0.984, blue: 0.992, alpha: 1) /* global color blue of weight 50 */
+    public static let colorGlobalColorBlue500 = UIColor(red: 0.098, green: 0.576, blue: 0.816, alpha: 1) /* global color blue of weight 500 */
+    public static let colorGlobalColorBlue600 = UIColor(red: 0.000, green: 0.467, blue: 0.698, alpha: 1) /* global color blue of weight 600 */
+    public static let colorGlobalColorBlue700 = UIColor(red: 0.000, green: 0.396, blue: 0.588, alpha: 1) /* global color blue of weight 700 */
+    public static let colorGlobalColorBlue800 = UIColor(red: 0.000, green: 0.318, blue: 0.475, alpha: 1) /* global color blue of weight 800 */
+    public static let colorGlobalColorBlue900 = UIColor(red: 0.000, green: 0.235, blue: 0.353, alpha: 1) /* global color blue of weight 900 */
+    public static let colorGlobalColorGreen100 = UIColor(red: 0.875, green: 0.953, blue: 0.863, alpha: 1) /* global color green of weight 100 */
+    public static let colorGlobalColorGreen200 = UIColor(red: 0.690, green: 0.882, blue: 0.667, alpha: 1) /* global color green of weight 200 */
+    public static let colorGlobalColorGreen300 = UIColor(red: 0.498, green: 0.808, blue: 0.478, alpha: 1) /* global color green of weight 300 */
+    public static let colorGlobalColorGreen400 = UIColor(red: 0.412, green: 0.678, blue: 0.396, alpha: 1) /* global color green of weight 400 */
+    public static let colorGlobalColorGreen50 = UIColor(red: 0.969, green: 0.984, blue: 0.922, alpha: 1) /* global color green of weight 50 */
+    public static let colorGlobalColorGreen500 = UIColor(red: 0.318, green: 0.514, blue: 0.306, alpha: 1) /* global color green of weight 500 */
+    public static let colorGlobalColorGreen600 = UIColor(red: 0.259, green: 0.404, blue: 0.247, alpha: 1) /* global color green of weight 600 */
+    public static let colorGlobalColorGreen700 = UIColor(red: 0.212, green: 0.322, blue: 0.200, alpha: 1) /* global color green of weight 700 */
+    public static let colorGlobalColorGreen800 = UIColor(red: 0.165, green: 0.243, blue: 0.157, alpha: 1) /* global color green of weight 800 */
+    public static let colorGlobalColorGreen900 = UIColor(red: 0.118, green: 0.169, blue: 0.114, alpha: 1) /* global color green of weight 900 */
+    public static let colorGlobalColorMint100 = UIColor(red: 0.773, green: 0.973, blue: 0.918, alpha: 1) /* global color mint of weight 100 */
+    public static let colorGlobalColorMint200 = UIColor(red: 0.333, green: 0.922, blue: 0.808, alpha: 1) /* global color mint of weight 200 */
+    public static let colorGlobalColorMint300 = UIColor(red: 0.000, green: 0.831, blue: 0.714, alpha: 1) /* global color mint of weight 300 */
+    public static let colorGlobalColorMint400 = UIColor(red: 0.039, green: 0.698, blue: 0.604, alpha: 1) /* global color mint of weight 400 */
+    public static let colorGlobalColorMint50 = UIColor(red: 0.580, green: 0.580, blue: 0.580, alpha: 1) /* global color mint of weight 50 */
+    public static let colorGlobalColorMint500 = UIColor(red: 0.094, green: 0.525, blue: 0.459, alpha: 1) /* global color mint of weight 500 */
+    public static let colorGlobalColorMint600 = UIColor(red: 0.102, green: 0.416, blue: 0.365, alpha: 1) /* global color mint of weight 600 */
+    public static let colorGlobalColorMint700 = UIColor(red: 0.098, green: 0.329, blue: 0.294, alpha: 1) /* global color mint of weight 700 */
+    public static let colorGlobalColorMint800 = UIColor(red: 0.090, green: 0.251, blue: 0.224, alpha: 1) /* global color mint of weight 800 */
+    public static let colorGlobalColorMint900 = UIColor(red: 0.078, green: 0.173, blue: 0.157, alpha: 1) /* global color mint of weight 900 */
+    public static let colorGlobalColorNeutral100 = UIColor(red: 0.929, green: 0.929, blue: 0.929, alpha: 1) /* global color neutral of weight 100 */
+    public static let colorGlobalColorNeutral200 = UIColor(red: 0.878, green: 0.878, blue: 0.878, alpha: 1) /* global color neutral of weight 200 */
+    public static let colorGlobalColorNeutral300 = UIColor(red: 0.831, green: 0.831, blue: 0.831, alpha: 1) /* global color neutral of weight 300 */
+    public static let colorGlobalColorNeutral400 = UIColor(red: 0.620, green: 0.620, blue: 0.620, alpha: 1) /* global color neutral of weight 400 */
+    public static let colorGlobalColorNeutral50 = UIColor(red: 0.988, green: 0.988, blue: 0.988, alpha: 1) /* global color neutral of weight 50 */
+    public static let colorGlobalColorNeutral500 = UIColor(red: 0.467, green: 0.467, blue: 0.467, alpha: 1) /* global color neutral of weight 500 */
+    public static let colorGlobalColorNeutral600 = UIColor(red: 0.369, green: 0.369, blue: 0.369, alpha: 1) /* global color neutral of weight 600 */
+    public static let colorGlobalColorNeutral700 = UIColor(red: 0.294, green: 0.294, blue: 0.294, alpha: 1) /* global color neutral of weight 700 */
+    public static let colorGlobalColorNeutral800 = UIColor(red: 0.224, green: 0.224, blue: 0.224, alpha: 1) /* global color neutral of weight 800 */
+    public static let colorGlobalColorNeutral900 = UIColor(red: 0.157, green: 0.157, blue: 0.157, alpha: 1) /* global color neutral of weight 900 */
+    public static let colorGlobalColorNeutral950 = UIColor(red: 0.110, green: 0.110, blue: 0.118, alpha: 1) /* global color neutral of weight 950 */
+    public static let colorGlobalColorOrange100 = UIColor(red: 0.988, green: 0.922, blue: 0.863, alpha: 1) /* global color orange of weight 100 */
+    public static let colorGlobalColorOrange200 = UIColor(red: 0.976, green: 0.800, blue: 0.663, alpha: 1) /* global color orange of weight 200 */
+    public static let colorGlobalColorOrange300 = UIColor(red: 0.965, green: 0.678, blue: 0.463, alpha: 1) /* global color orange of weight 300 */
+    public static let colorGlobalColorOrange400 = UIColor(red: 0.890, green: 0.573, blue: 0.345, alpha: 1) /* global color orange of weight 400 */
+    public static let colorGlobalColorOrange50 = UIColor(red: 0.996, green: 0.976, blue: 0.961, alpha: 1) /* global color orange of weight 50 */
+    public static let colorGlobalColorOrange500 = UIColor(red: 0.753, green: 0.482, blue: 0.294, alpha: 1) /* global color orange of weight 500 */
+    public static let colorGlobalColorOrange600 = UIColor(red: 0.616, green: 0.388, blue: 0.243, alpha: 1) /* global color orange of weight 600 */
+    public static let colorGlobalColorOrange700 = UIColor(red: 0.522, green: 0.329, blue: 0.208, alpha: 1) /* global color orange of weight 700 */
+    public static let colorGlobalColorOrange800 = UIColor(red: 0.420, green: 0.263, blue: 0.169, alpha: 1) /* global color orange of weight 800 */
+    public static let colorGlobalColorOrange900 = UIColor(red: 0.310, green: 0.192, blue: 0.125, alpha: 1) /* global color orange of weight 900 */
+    public static let colorGlobalColorRed100 = UIColor(red: 1.000, green: 0.914, blue: 0.898, alpha: 1) /* global color red of weight 100 */
+    public static let colorGlobalColorRed200 = UIColor(red: 1.000, green: 0.780, blue: 0.749, alpha: 1) /* global color red of weight 200 */
+    public static let colorGlobalColorRed300 = UIColor(red: 0.988, green: 0.651, blue: 0.612, alpha: 1) /* global color red of weight 300 */
+    public static let colorGlobalColorRed400 = UIColor(red: 0.953, green: 0.478, blue: 0.451, alpha: 1) /* global color red of weight 400 */
+    public static let colorGlobalColorRed50 = UIColor(red: 1.000, green: 0.984, blue: 0.984, alpha: 1) /* global color red of weight 50 */
+    public static let colorGlobalColorRed500 = UIColor(red: 0.729, green: 0.353, blue: 0.337, alpha: 1) /* global color red of weight 500 */
+    public static let colorGlobalColorRed600 = UIColor(red: 0.576, green: 0.282, blue: 0.275, alpha: 1) /* global color red of weight 600 */
+    public static let colorGlobalColorRed700 = UIColor(red: 0.455, green: 0.227, blue: 0.227, alpha: 1) /* global color red of weight 700 */
+    public static let colorGlobalColorRed800 = UIColor(red: 0.341, green: 0.176, blue: 0.176, alpha: 1) /* global color red of weight 800 */
+    public static let colorGlobalColorRed900 = UIColor(red: 0.231, green: 0.125, blue: 0.125, alpha: 1) /* global color red of weight 900 */
+    public static let colorGlobalColorViolet100 = UIColor(red: 0.925, green: 0.929, blue: 0.980, alpha: 1) /* global color violet of weight 100 */
+    public static let colorGlobalColorViolet200 = UIColor(red: 0.816, green: 0.824, blue: 0.953, alpha: 1) /* global color violet of weight 200 */
+    public static let colorGlobalColorViolet300 = UIColor(red: 0.714, green: 0.722, blue: 0.922, alpha: 1) /* global color violet of weight 300 */
+    public static let colorGlobalColorViolet400 = UIColor(red: 0.584, green: 0.596, blue: 0.878, alpha: 1) /* global color violet of weight 400 */
+    public static let colorGlobalColorViolet50 = UIColor(red: 0.988, green: 0.988, blue: 0.996, alpha: 1) /* global color violet of weight 50 */
+    public static let colorGlobalColorViolet500 = UIColor(red: 0.408, green: 0.435, blue: 0.784, alpha: 1) /* global color violet of weight 500 */
+    public static let colorGlobalColorViolet600 = UIColor(red: 0.341, green: 0.341, blue: 0.612, alpha: 1) /* global color violet of weight 600 */
+    public static let colorGlobalColorViolet700 = UIColor(red: 0.286, green: 0.271, blue: 0.478, alpha: 1) /* global color violet of weight 700 */
+    public static let colorGlobalColorViolet800 = UIColor(red: 0.224, green: 0.204, blue: 0.353, alpha: 1) /* global color violet of weight 800 */
+    public static let colorGlobalColorViolet900 = UIColor(red: 0.161, green: 0.145, blue: 0.235, alpha: 1) /* global color violet of weight 900 */
+    public static let colorGlobalColorYellow100 = UIColor(red: 0.992, green: 0.925, blue: 0.765, alpha: 1) /* global color yellow of weight 100 */
+    public static let colorGlobalColorYellow200 = UIColor(red: 0.941, green: 0.827, blue: 0.420, alpha: 1) /* global color yellow of weight 200 */
+    public static let colorGlobalColorYellow300 = UIColor(red: 0.831, green: 0.733, blue: 0.373, alpha: 1) /* global color yellow of weight 300 */
+    public static let colorGlobalColorYellow400 = UIColor(red: 0.694, green: 0.616, blue: 0.318, alpha: 1) /* global color yellow of weight 400 */
+    public static let colorGlobalColorYellow50 = UIColor(red: 1.000, green: 0.988, blue: 0.961, alpha: 1) /* global color yellow of weight 50 */
+    public static let colorGlobalColorYellow500 = UIColor(red: 0.518, green: 0.467, blue: 0.247, alpha: 1) /* global color yellow of weight 500 */
+    public static let colorGlobalColorYellow600 = UIColor(red: 0.408, green: 0.369, blue: 0.204, alpha: 1) /* global color yellow of weight 600 */
+    public static let colorGlobalColorYellow700 = UIColor(red: 0.325, green: 0.294, blue: 0.169, alpha: 1) /* global color yellow of weight 700 */
+    public static let colorGlobalColorYellow800 = UIColor(red: 0.243, green: 0.224, blue: 0.133, alpha: 1) /* global color yellow of weight 800 */
+    public static let colorGlobalColorYellow900 = UIColor(red: 0.169, green: 0.157, blue: 0.098, alpha: 1) /* global color yellow of weight 900 */
+    public static let contentGlobalFontFontFamilyContent = "Inter" /* Font family for content fonts */
+    public static let contentGlobalFontFontFamilyDisplay = "Inter" /* Font family for display fonts */
+    public static let contentGlobalFontFontWeightBlack = "Black" /* heaviest weight for fonts */
+    public static let contentGlobalFontFontWeightBold = "Bold" /* bold weight for fonts */
+    public static let contentGlobalFontFontWeightDefault = "Regular" /* default weight for fonts */
+    public static let contentGlobalFontFontWeightSemiBold = "Semi Bold" /* semibold weight for fonts */
+}
